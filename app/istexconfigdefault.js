354a20ac9ef00f18d362f53428c0066f5d3972c7
thomasfrantz <thom.frantz@gmail.com>
2015-07-06 17:33:57 +0200

    mini-corrections

--- app/istexconfigdefault.js
+++ app/istexconfigdefault.js
@@ -16,7 +16,7 @@ app.run(['$rootScope', function($rootScope) {
         focusInputQueryOnLoad: false,
 
         // il est possible de ne charger que certaines facettes
-        // par défaut, on charge seulement : 'corpus','pubdate','copyrightdate'
+        // par défaut, on charge seulement : 'corpus','pubdate','copyrightdate','score'
         facetsToLoad: [ 'corpusName','publicationDate','copyrightDate', 'score'],
 
         // il n'est possible de charger que certains champs de la recherche avancée
