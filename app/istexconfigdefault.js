0191cb0c15ef5811bd8ce87a85445768444b4c08
thomasfrantz <thom.frantz@gmail.com>
2015-07-16 12:13:16 +0200

    mini correction and update of exemple2 that uses a proxy

--- app/istexconfigdefault.js
+++ app/istexconfigdefault.js
@@ -2,10 +2,11 @@ app.run(['$rootScope', function($rootScope) {
 
     $rootScope.istexConfigDefault = {
         // l'adresse de l'API de l'Istex
+        istexApi: 'https://api.istex.fr',
+        // on peut avoir besoin de proxyfier les liens vers les plein-textes ou les méta-données
         // pour une ezproxyfication, réglez ici proxyApi à l'adresse ezproxyfiée
         // ex à l'UL: https://api-istex-fr.bases-doc.univ-lorraine.fr
-        istexApi: 'https://api.istex.fr',
-        proxyApi: 'https://api-istex-fr.bases-doc.univ-lorraine.fr',
+        proxyApi: 'https://api.istex.fr',
 
         // pour lancer une recherche au chargement de la page
         // indiquer les mots à rechercher (argument de ?q= au niveau de l'api istex)
