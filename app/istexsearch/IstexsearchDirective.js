77ff49b7576355fa20690344368ce7c0d2ba6b1c
thomasfrantz <thom.frantz@gmail.com>
2015-10-16 12:17:52 +0200

    CSS and JS layout modified

--- app/istexsearch/IstexsearchDirective.js
+++ app/istexsearch/IstexsearchDirective.js
@@ -4,18 +4,20 @@ app.directive('istexSearch', function () {
         '<div id="istex-widget-search" ng-controller="IstexsearchCtrl" >' +
             '<form class="istex-search-form" >'+
                 '<div class="istex-search-bar-wrapper">'+
-                    '<input class="istex-search-submit" type="submit" value="Rechercher" ng-click="search()">'+
                     '<span><input class="istex-search-input" type="search" value="" placeholder="{{istexConfigDefault.labels.search[\'placeholder\'] || \'Votre requête ici ...\'}}" ng-model="query" ng-focus="istexConfigDefault.focusInputQueryOnLoad"></span>'+
+                    '<input class="istex-search-submit" type="submit" value="Rechercher" ng-click="search()">'+
                 '</div>' +
                 '<div class="istex-advanced-wrapper" ng-if="istexConfigDefault.advancedToLoad">' +
-                    '<div>' +
-                        '<a href="" ng-click="toggleAdvanced()">{{ (istexConfigDefault.labels.search["advancedTitle"] || "Recherche avancée") | capitalize }}</a>' +
+                    '<div class="istex-advanced-button" ng-click="toggleAdvanced()">' +
+                        '<a href="" >{{ (istexConfigDefault.labels.search["advancedTitle"] || "Recherche avancée") | capitalize }}</a>' +
                     '</div>' +
+                    '<div class="istex-advanced-inputs">'+
                     '<div class="istex-advanced" ng-repeat="(advancedName, advanced) in advancedQuery" ng-show="showAdvanced">' +
                         '<h4 class="istex-advanced-name">{{ (istexConfigDefault.labels.search[advancedName] || advancedName) | capitalize }}</h4>'+
                         '<div class="istex-advanced-{{advancedName}}" >'+
                             '<input type="search" ng-model="advancedQuery[advancedName]">'+
                         '</div>'+
+                    '</div>'+
                     '</div>'+
                 '</div>' +
                 '<p class="istex-search-error"></p>'+
