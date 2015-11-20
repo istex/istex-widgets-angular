4f101a712f15e7d5c37593fdbf5bf89d977cb5f4
thomasfrantz <thom.frantz@gmail.com>
2015-11-20 17:09:15 +0100

    loading gif part 1

--- app/istexresults/IstexresultsDirective.js
+++ app/istexresults/IstexresultsDirective.js
@@ -2,7 +2,7 @@ app.directive('istexResults', function () {
     return {
         template:'' +
         '<div class="istex-results-noresult" ng-show="noresult">'+
-            '{{istexConfigDefault.labels.results.noresult || "Il n\'y a pas de résultat à afficher chaaaa !"}}'+
+            '{{istexConfigDefault.labels.results.noresult || "Il n\'y a pas de résultat à afficher !"}}'+
         '</div>'+
         '<div class="istex-hidebutton" ng-click="istexConfigDefault.hideButton = false;" ng-show="!noresult && istexConfigDefault.hideButton" title="{{istexConfigDefault.labels.results.showResult || \'Cliquez pour afficher les résultats\'}}">{{ total || ". . . . . . . . ." | numberize }} documents</div>'+
         '<div id="istex-widget-results" style="opacity: 1;" ng-controller="IstexresultsCtrl" ng-toggle="showResults" ng-show="!noresult && !istexConfigDefault.hideButton">'+
