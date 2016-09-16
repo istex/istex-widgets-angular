fddbee4e4a3f658f5a9e8446999b5a691c90862b
Rémy MEJA <remy.meja@inist.fr>
2016-09-16 08:30:21 +0200

    Use 'templateUrl' fot better management templates

--- app/istexauth/IstexauthDirective.js
+++ app/istexauth/IstexauthDirective.js
@@ -1,8 +1,5 @@
 app.directive('istexAuth', function () {
     return {
-        template:'' +
-        '<div id="istex-widget-auth" ng-controller="IstexauthCtrl">'+
-            '<button class="istex-ezproxy-auth-btn" ng-if="!isConnected" ng-click="connect()">Se connecter<div></div></button>'+
-        '</div>'
+        templateUrl: './../app/istexauth/IstexauthTemplate.html'
     };
 });
